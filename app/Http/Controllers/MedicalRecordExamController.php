889daf50496d2c2682898b987d9b7a2c80b08ac3
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2026-06-07 02:59:16 -0300

    style: padronizar código com Laravel Pint

--- app/Http/Controllers/MedicalRecordExamController.php
+++ app/Http/Controllers/MedicalRecordExamController.php
@@ -2,17 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\MedicalRecordExam\StoreMedicalRecordExamRequest;
 use App\Http\Resources\MedicalRecordExamResource;
 use App\Services\MedicalRecordExamService;
-use Illuminate\Http\Request;
 
 class MedicalRecordExamController extends Controller
 {
     public function __construct(
         protected MedicalRecordExamService $medicalRecordExamService
-    ){}
+    ) {}
 
     public function store(StoreMedicalRecordExamRequest $request)
     {
--- app/Http/Controllers/MedicalRecordPhysicalExamController.php
+++ app/Http/Controllers/MedicalRecordPhysicalExamController.php
@@ -2,18 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\MedicalRecordPhysicalExam\StoreMedicalRecordPhysicalExamRequest;
 use App\Http\Resources\MedicalRecordPhysicalExamResource;
-use App\Models\MedicalRecordPhysicalExam;
 use App\Services\MedicalRecordPhysicalExamService;
-use Illuminate\Http\Request;
 
 class MedicalRecordPhysicalExamController extends Controller
 {
     public function __construct(
-       protected MedicalRecordPhysicalExamService $medicalRecordPhysicalExam,
-    ){}
+        protected MedicalRecordPhysicalExamService $medicalRecordPhysicalExam,
+    ) {}
 
     public function store(StoreMedicalRecordPhysicalExamRequest $request)
     {
